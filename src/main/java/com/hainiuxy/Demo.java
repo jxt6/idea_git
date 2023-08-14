80d2933b7bae227f8aa13fa6d7349409721e40ba
jxt6 <hainiu@example.com>
2023-08-14 21:18:36 +0800

    git项目首次提交

--- src/main/java/com/hainiuxy/Demo.java
+++ src/main/java/com/hainiuxy/Demo.java
@@ -1,4 +1,8 @@
 package com.hainiuxy;
 
 public class Demo {
+    public String say(String message) {
+        System.out.println("hello " + message);
+        return "Hello, " + message;
+    }
 }
--- src/test/java/com/hainiuxy/DemoTest.java
+++ src/test/java/com/hainiuxy/DemoTest.java
@@ -1,4 +1,8 @@
 package com.hainiuxy;
 
 public class DemoTest  {
+    public static void main(String[] args) {
+        Demo demo = new Demo();
+        demo.say("git");
+    }
 }
